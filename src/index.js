91c3c891d34cad5c0eeacf8ca5bd5a2a99f78515
julian <julian7454@gmail.com>
2019-02-17 17:09:51 +0800

    init hello saga

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { helloSaga } from "./sagas";
+
+import reducer from "./reducers";
 
 import "./styles.css";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(helloSaga);
+
+const action = type => store.dispatch({ type });
 
 function App() {
   return <div className="App" />;
